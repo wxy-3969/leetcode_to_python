24b89b0bce2bc9685c9a444e71b5909e398d401d
wxy <154322658+wxy-3969@users.noreply.github.com>
2024-11-12 19:50:44 +0800

    02两数相加

--- 02两数相加.py
+++ 02两数相加.py
@@ -22,6 +22,7 @@ def addTwoNumbers(self, l1, l2):
         # 创建新节点并更新当前节点的值
         current.next = NodeList(total % 10)
         current = current.next
+        
         # 移动l1和l2指针到下一个节点（如果存在）
         l1 = l1.next if l1 else None
         l2 = l2.next if l2 else None
